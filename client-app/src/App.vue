8889bfeafaba87bf85f21a56b52d0ff0cf43f7aa
YonchevSimeon <simeonyonchev.com>
2021-09-12 15:25:43 +0300

    vuejs config

---
+++ client-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <header class="app-header"></header>
+    <section class="app-contnet">
+      <router-view />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style></style>
