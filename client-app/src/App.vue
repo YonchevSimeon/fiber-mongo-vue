6f0c99671c9813f8b37a37e06826078239e7fb76
YonchevSimeon <simeonyonchev@abv.bg>
2021-10-05 15:34:52 +0300

    added title and navigation on the nav-drawer

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <v-app>
-    <v-navigation-drawer app> </v-navigation-drawer>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Menu </v-list-item-title>
+          <v-list-item-subtitle> options </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          router
+          :to="item.route"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar app color="primary accent-20" dense dark>
       <v-spacer></v-spacer>
@@ -42,10 +66,21 @@
 import { mapGetters } from "vuex";
 export default {
   name: "App",
+  data() {
+    return {
+      items: [
+        { title: "Posts", icon: "mdi-view-dashboard", route: "/" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+    };
+  },
   computed: {
     ...mapGetters(["isLoggedIn"]),
   },
   methods: {
+    async logout() {
+      // TOOD Logout user
+    },
     async navToRegister() {
       this.$router.push({
         name: "register",
@@ -54,6 +89,11 @@ export default {
     async navToLogin() {
       this.$router.push({
         name: "login",
+      });
+    },
+    async navToPosts() {
+      this.$router.push({
+        name: "home",
       });
     },
   },
